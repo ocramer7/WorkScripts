0f1c2538d594a2dd2d248e692f1bd2462a3625c2
ocramer7 <ocramer7@gmail.com>
2024-09-06 12:53:40 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import subprocess
+import sys
+import webbrowser
+
+print(webbrowser._browsers) # List available browsers
+print("OS Platform:", sys.platform)
+
+
+
+urls = ['https://mail.google.com', 'https://support.sdsmt.edu/TDNext/Home/Desktop/Default.aspx',
+        'https://support.sdsmt.edu/TDClient/30/Portal/Home/?ToUrl=',
+        'https://ngsp.lenovo.com/dashboard',
+        'https://sdmines.sdsmt.edu/cgi-bin/global/adprotected/its_review_tablet_repairs.cgi?reviewid=95&perpage=25&pagenum=1']
+
+
+def windows_os(urls):
+    # Windows spesific code here
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+    for url in urls:
+        webbrowser.get(chrome_path).open(url,new=1)
+
+
+def macos_os(urls):
+    chrome_path = 'open -a /Applications/Google\\ Chrome.app %s'
+    # outlook_path = 'open -a /Applications/Microsoft Outlook.app'
+    subprocess.Popen(['open', '/Applications/Microsoft Outlook.app/'])
+    for url in urls:
+        webbrowser.get(chrome_path).open(url,new=1)
+# Linux to be added later
+# def linux_os():
+#     chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+#     return webbrowser.get(chrome_path).open(url)
+
+
+if sys.platform.startswith('darwin'):
+    macos_os(urls)
+elif sys.platform.startswith('win32'):
+    windows_os(urls)
+# elif sys.platform.startswith('linux'):
+#     linux_os()
+else:
+    print('Unsupported operating system')
+    exit(0)
